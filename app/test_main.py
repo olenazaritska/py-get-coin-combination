9922902b74f13085b8033cea39e0eb298d12d4c8
Olena Zaritska <zaritskaos@gmail.com>
2024-05-21 00:45:55 +0300

    implement py-get-coin-combination

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,38 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+class TestGetCoinCombination:
+    @pytest.mark.parametrize(
+        "cents, expected_combination",
+        [
+            pytest.param(
+                1,
+                [1, 0, 0, 0],
+                id="should have correct num of pennies"
+            ),
+            pytest.param(
+                6,
+                [1, 1, 0, 0],
+                id="should have correct num of pennies and nickels"
+            ),
+            pytest.param(
+                17,
+                [2, 1, 1, 0],
+                id="should have correct num of pennies, nickels and dimes"
+            ),
+            pytest.param(
+                50,
+                [0, 0, 0, 2],
+                id="should have correct num of pennies, nickels, "
+                   "dimes and quarters"
+            )
+        ]
+    )
+    def test_correct_split_into_coins(
+            self,
+            cents: int,
+            expected_combination: list
+    ) -> None:
+        assert get_coin_combination(cents) == expected_combination
