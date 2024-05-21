233ead1517cedb2137d7af48eb618e87c2b2022e
Olena Zaritska <zaritskaos@gmail.com>
2024-05-21 10:22:37 +0300

    implement py-get-coin-combination v2

--- app/test_main.py
+++ app/test_main.py
@@ -4,30 +4,53 @@ from app.main import get_coin_combination
 
 
 class TestGetCoinCombination:
+    def test_coins_options_should_be_only_4(self) -> None:
+        assert len(get_coin_combination(5)) == 4
+
     @pytest.mark.parametrize(
         "cents, expected_combination",
         [
             pytest.param(
+                0,
+                [0, 0, 0, 0],
+                id="should have only pennies"
+            ),
+            pytest.param(
                 1,
                 [1, 0, 0, 0],
-                id="should have correct num of pennies"
+                id="should have only pennies"
             ),
             pytest.param(
                 6,
                 [1, 1, 0, 0],
-                id="should have correct num of pennies and nickels"
+                id="should have pennies and nickels"
             ),
             pytest.param(
                 17,
                 [2, 1, 1, 0],
-                id="should have correct num of pennies, nickels and dimes"
+                id="should have pennies, nickels and dimes"
+            ),
+            pytest.param(
+                41,
+                [1, 1, 1, 1],
+                id="should have pennies, nickels, dimes and quarters"
             ),
             pytest.param(
                 50,
                 [0, 0, 0, 2],
-                id="should have correct num of pennies, nickels, "
-                   "dimes and quarters"
-            )
+                id="should have quarters only"
+            ),
+            pytest.param(
+                35,
+                [0, 0, 1, 1],
+                id="should have dimes and quarters"
+            ),
+            pytest.param(
+                40,
+                [0, 1, 1, 1],
+                id="should have nickels, dimes and quarters"
+            ),
+
         ]
     )
     def test_correct_split_into_coins(
